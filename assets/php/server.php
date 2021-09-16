9efb23f9708a2bcdee9b941d78f9abf5c794bd90
LavederMark-Ylann <markylannlaveder@gmail.com>
2021-09-16 09:27:52 +0200

    fix

--- assets/php/server.php
+++ assets/php/server.php
@@ -41,8 +41,8 @@ if (isset($postdata) && !empty($postdata)) {
   $mail->isSMTP();
   $mail->Host = 'smtp.gmail.com';
   $mail->SMTPAuth = true;
-  $mail->Username = 'sendmaildevelopmentaplus@gmail.com';
-  $mail->Password = 'NgXg!i&4@LKs0UzWxy';
+  $mail->Username = '';
+  $mail->Password = '';
   $mail->SMTPSecure = 'tls';
   $mail->Port = 587;
 
